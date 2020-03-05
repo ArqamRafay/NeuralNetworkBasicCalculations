5330611dfdcb3a9b5624c7932b3e3a0bfd18acda
Arqam Rafay <getarqamnow@hotmail.com>
2020-03-05 11:22:08 +0500

    add line of code

---
+++ NeuralNetwrok.py
@@ -0,0 +1,68 @@
+## https://github.com/iamtrask/Grokking-Deep-Learning/blob/master/Chapter3%20-%20%20Forward%20Propagation%20-%20Intro%20to%20Neural%20Prediction.ipynb
+## A Simple Neural Network Making a Prediction
+## What is a Neural Network?
+## The network:
+# weight = 0.1 
+# def neural_network(input, weight):
+#     prediction = input * weight
+#     return prediction
+
+# How we use the network to predict something:
+# number_of_toes = [8.5, 9.5, 10, 9]  # Explain with example 
+# input = number_of_toes[0]
+# pred = neural_network(input,weight)
+# print(pred)
+
+
+# # Making a Prediction with Multiple Inputs
+# def w_sum(a,b):
+#     assert(len(a) == len(b))
+#     output = 0
+#     for i in range(len(a)):
+#         output += (a[i] * b[i])
+    # return output
+
+# weights = [0.1, 0.2, 0] 
+    
+# def neural_network(input, weights):
+#     pred = w_sum(input,weights)
+#     return pred
+
+# toes = current number of toes
+# wlrec = current games won (percent)
+# nfans = fan count (in millions)
+
+# toes =  [8.5, 9.5, 9.9, 9.0]
+# wlrec = [0.65, 0.8, 0.8, 0.9]
+# nfans = [1.2, 1.3, 0.5, 1.0]
+
+# Input corresponds to every entry
+# for the first game of the season.
+
+# input = [toes[0],wlrec[0],nfans[0]]
+# pred = neural_network(input,weights)
+# print(pred)
+
+# Same above code using NumPy
+import numpy as np
+weights = np.array([0.1, 0.2, 0])
+def neural_network(input, weights):
+    pred = input.dot(weights)
+    return pred
+    
+toes =  np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
+
+# Input corresponds to every entry
+# for the first game of the season.
+
+input = np.array([toes[0],wlrec[0],nfans[0]])
+pred = neural_network(input,weights)
+print(pred)
+
+
+
+
+
+
